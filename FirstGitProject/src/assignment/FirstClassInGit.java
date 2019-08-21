8f3bacdd08f106544e7813f92ef2634998548530
nj01585 <nj01585@CEIT2553214X005.ad.georgiasouthern.edu>
2019-08-21 16:43:35 -0400

    Added scanner and print line for last name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,5 +1,5 @@
 package assignment;
-
+import java.util.Scanner;
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
@@ -7,6 +7,10 @@ public class FirstClassInGit {
 		
 		//add a statement to display your name in the console
 		System.out.println("Hello! My name is Nailah.");
+		System.out.println("What is your last name?");
+		Scanner sc = new Scanner(System.in);
+		String lastName = sc.next();
+		System.out.println("Your last name is: " + lastName);
 		
 		//Then commit your changes
 
