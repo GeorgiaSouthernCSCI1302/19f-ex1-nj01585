e8dd4efc238c101a0d767d48c6235b60c70feaf8
nj01585 <nj01585@CEIT2553214X005.ad.georgiasouthern.edu>
2019-08-21 16:34:19 -0400

    added sysout message line 9

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -6,6 +6,7 @@ public class FirstClassInGit {
 		System.out.println("Hello World!");
 		
 		//add a statement to display your name in the console
+		System.out.println("Hello! My name is Nailah.");
 		
 		//Then commit your changes
 
